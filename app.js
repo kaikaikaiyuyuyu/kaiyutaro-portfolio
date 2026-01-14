19db30dbee82b67ae76d2dd281dba0f0c5a55939
kaiyutaro <130325980+kaiyutaro@users.noreply.github.com>
2026-01-14 09:26:27 +0900

    fix: install express

--- app.js
+++ app.js
@@ -31,4 +31,9 @@ app.get("/projects/:slug", (req, res) => {
   res.render("projects/show", { title: project.title, project });
 });
 
-app.listen(3000, () => console.log("http://localhost:3000"));
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
+
